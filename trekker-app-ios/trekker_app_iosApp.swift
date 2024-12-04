6cf7f73fe3ff1e9f6937d1812885a298acbb3b3d
Hammad335 <hamadmemon946@gmail.com>
2024-12-04 11:40:46 +0500

    Initial Commit

---
+++ trekker-app-ios/trekker_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  trekker_app_iosApp.swift
+//  trekker-app-ios
+//
+//  Created by Hamad on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct trekker_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trekker-app-iosTests/trekker_app_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  trekker_app_iosTests.swift
+//  trekker-app-iosTests
+//
+//  Created by Hamad on 04/12/2024.
+//
+
+import Testing
+@testable import trekker_app_ios
+
+struct trekker_app_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
